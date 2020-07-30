988ba8b1a70d22635f2215172e3093a8897f5fbf
Connor Bartal <connorbartal@Connors-MacBook-Pro.local>
2020-07-30 18:32:15 -0400

    fixed home page resizing

---
+++ scripts/adjustIndex.js
@@ -0,0 +1,45 @@
+
+
+window.onresize = function() {
+    var image = document.getElementById("background_image");
+    var height = image.height;
+    document.getElementById("background_image_overlay").setAttribute("style", "height:" + height + "px");
+    document.getElementById("background_image_overlay").style.height = height + "px";
+
+    var width = image.width;
+    if (width < 515) {
+        document.getElementById("title").setAttribute("style", "font-size:30px");
+        document.getElementById("title").style.fontSize = "30px";
+        document.getElementById("typingtext").setAttribute("style", "font-size:15px");
+        document.getElementById("typingtext").style.fontSize = "15px";
+    }
+    else {
+        document.getElementById("title").setAttribute("style", "font-size:80px");
+        document.getElementById("title").style.fontSize = "80px";
+        document.getElementById("typingtext").setAttribute("style", "font-size:50px");
+        document.getElementById("typingtext").style.fontSize = "50px";
+
+    }
+}
+
+window.onload = function() {
+    var image = document.getElementById("background_image");
+    var height = image.height;
+    document.getElementById("background_image_overlay").setAttribute("style", "height:" + height + "px");
+    document.getElementById("background_image_overlay").style.height = height + "px";
+
+    var width = image.width;
+    if (width < 515) {
+        document.getElementById("title").setAttribute("style", "font-size:30px");
+        document.getElementById("title").style.fontSize = "30px";
+        document.getElementById("typingtext").setAttribute("style", "font-size:15px");
+        document.getElementById("typingtext").style.fontSize = "15px";
+    }
+    else {
+        document.getElementById("title").setAttribute("style", "font-size:80px");
+        document.getElementById("title").style.fontSize = "80px";
+        document.getElementById("typingtext").setAttribute("style", "font-size:50px");
+        document.getElementById("typingtext").style.fontSize = "50px";
+
+    }
+}
