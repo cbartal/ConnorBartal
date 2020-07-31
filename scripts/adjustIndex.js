2a1a4f4aee7f3f04849be281957575cfd7603b1e
cbartal <cbartal@vt.edu>
2020-07-31 14:24:33 -0400

    finished home page

--- scripts/adjustIndex.js
+++ scripts/adjustIndex.js
@@ -1,30 +1,17 @@
 window.onresize = function() {
-    var image = document.getElementById("background_image");
-    var height = image.height;
-    document.getElementById("background_image_overlay").setAttribute("style", "height:" + height + "px");
-    document.getElementById("background_image_overlay").style.height = height + "px";
-
-    var width = image.width;
-    if (width < 515) {
-        document.getElementById("title").setAttribute("style", "font-size:30px");
-        document.getElementById("title").style.fontSize = "30px";
-        document.getElementById("typingtext").setAttribute("style", "font-size:15px");
-        document.getElementById("typingtext").style.fontSize = "15px";
-    } else {
-        document.getElementById("title").setAttribute("style", "font-size:80px");
-        document.getElementById("title").style.fontSize = "80px";
-        document.getElementById("typingtext").setAttribute("style", "font-size:50px");
-        document.getElementById("typingtext").style.fontSize = "50px";
-    }
-    height = height + 35;
-    document.getElementById("emptydiv").style.height = height + "px";
-    document.getElementById("emptydiv").setAttribute("style", "height:" + height + "px");
-
+    backgroundFormat();
     styleSocialMedia();
+    riverImageFix();
 
 }
 
 window.onload = function() {
+    backgroundFormat();
+    styleSocialMedia();
+    riverImageFix();
+}
+
+function backgroundFormat() {
     var image = document.getElementById("background_image");
     var height = image.height;
     document.getElementById("background_image_overlay").setAttribute("style", "height:" + height + "px");
@@ -41,14 +28,12 @@ window.onload = function() {
         document.getElementById("title").style.fontSize = "80px";
         document.getElementById("typingtext").setAttribute("style", "font-size:50px");
         document.getElementById("typingtext").style.fontSize = "50px";
-
     }
     height = height + 35;
     document.getElementById("emptydiv").style.height = height + "px";
     document.getElementById("emptydiv").setAttribute("style", "height:" + height + "px");
-    styleSocialMedia();
-}
 
+}
 
 function styleSocialMedia() {
     var width = window.innerWidth / 13;
@@ -56,6 +41,17 @@ function styleSocialMedia() {
     var i;
     for (i = 0; i < x.length; i++) {
         x[i].style.width = width + "px";
-        x[i].style.margin = (width / 3) + "px";
+        x[i].style.margin = (width / 4.5) + "px";
+    }
+}
+
+function riverImageFix() {
+    if (window.innerWidth < 510) {
+        document.getElementById("riverimage").style.width = window.innerWidth + "px";
+        document.getElementById("riverphoto").style.width = "100%";
+    }
+    else {
+        document.getElementById("riverimage").style.width = "35%";
+        document.getElementById("riverphoto").style.width = "95%";
     }
 }
